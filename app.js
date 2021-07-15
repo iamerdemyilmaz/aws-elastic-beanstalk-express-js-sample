a51d2cc482cffb9237b34ca043f68c696d1059bd
Erdem Yilmaz <erdem@Erdems-MacBook-Pro.local>
2021-07-15 13:28:45 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Erdem!'));
+app.get('/', (req, res) => res.send('YOU CAN DO THIS!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
